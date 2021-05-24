5bcf735776bead1549209465590f78c4b7125a15
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-05-24 13:34:00 -0500

    UpTask: validación simple de campo que da cuerpo al formulario

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -5,13 +5,34 @@ exports.proyectosHome = (req, res) => {
     });
 }
 
-exports.formularioProyecto = (req, res) =>{
+exports.formularioProyecto = (req, res) => {
     res.render('nuevo-proyecto', {
         nombrePagina: 'Nuevo Proyecto'
     });
 }
 
 exports.nuevoProyecto = (req, res) => {
-    // Enviar a la consla lo que el usuario escriba
-    console.log(req.body);
+    // Enviar a la consola lo que el usuario escriba
+    //console.log(req.body);
+
+    //Validar campo en el input
+    const { nombre } = req.body;
+
+    let errores = [];
+
+    if (!nombre) {
+        errores.push({ 'texto': 'Agrega un nombre al proyecto' })
+    }
+
+    // Si hay errores
+    if(errores.length > 0 ){
+        res.render('nuevo-proyecto',{
+            nombrePagina: 'Nuevo Proyecto',
+            errores
+        })
+    }else {
+        //No hay errores
+        //Insertar en la base de datos
+    }
+
 }
