91b322bc0df4c0b87f6a0ff8a983ffddede842d3
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-06-07 15:05:24 -0500

    UpTask: almacenando las tareas en la base de datos

--- controllers/tareasController.js
+++ controllers/tareasController.js
@@ -1,7 +1,37 @@
 
-const Tareas  = require('../models/Tareas');
+const Proyectos = require('../models/Proyectos');
+const Tareas = require('../models/Tareas');
 
 
-exports.agregarTarea = (req, res) => {
-    res.send('Enviado...');
+exports.agregarTarea = async (req, res, next) => {
+    //res.send('Enviado...');
+    //console.log(req.params.url);
+
+    /*
+    * findOne = "SELECT * FROM proyectos WHERE id = 20 LIMIT 1;"
+    */
+    //Obtenmos el pryecto actual
+    const proyecto = await Proyectos.findOne({ where: { url: req.params.url } });
+
+    /* Verfiviando la validez de la consulta
+     console.log(proyecto);
+    console.log(req.body);  - > musetra la url qde l trea declarada en al vista, en el formulario en el input.tarea*/
+
+    //Leer el valor el input
+    const { tarea } = req.body;
+
+    //estado 0 = incompleto y ID del proyecto
+    const estado = 0;
+    const proyectoId = proyecto.id;
+
+    //Insertar en la base de datos
+    const resultado = await Tareas.create({tarea, estado, proyectoId});
+
+
+    if(!resultado){
+        return next();
+    }
+
+    //Redireccionar
+    res.redirect(`/proyectos/${req.params.url}`);
 }
